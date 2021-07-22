773f17cb4912a3973fd811215687b4615b8b202f
Josh <52624863+joshwilliammoore@users.noreply.github.com>
2021-07-22 22:40:38 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navbar msg="d v" />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Index msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import Index from './components/Index.vue'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Index,
+    Navbar,
+  }
+}
+</script>
---
+++ src/components/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="title">{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import './assets/scss/_shared.scss';
+
+</style>
